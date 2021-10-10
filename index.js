8a26d38346410be820d408307559402f063dd83b
Brinda Behal <brindaxo5@gmail.com>
2021-10-10 13:06:54 -0400

    commit tree 22282c97207095c2f845d59b7bba059b0c9b97f0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(names, events){
+    let cards = [] // empty array to store new thank you cards to return the array
+    for (let i = 0; i < names.length; i ++ ){ // start at 0 | go through every elemetnt in the names array 
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`) // 
+    }
+    return cards
+}
+
+
+
+
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
